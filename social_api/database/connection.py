01728a06415c264f30e3bf4f63bc707fc4b290ed
HicaroD <hdanrlley1@gmail.com>
2023-04-20 13:35:58 -0300

    feature(database-connection): example class for connecting to local neo4j db

--- social_api/database/connection.py
+++ social_api/database/connection.py
@@ -1,3 +1,27 @@
+from neo4j import GraphDatabase
+
+# TODO: build a context manager with this class for interacting with the DB
 class SocialAppDatabase:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+    
+    def print_greeting(self, message):
+        with self.driver.session() as session:
+            greeting = session.execute_write(self._create_and_return_greeting, message)
+            print(greeting)
+
+    @staticmethod
+    def _create_and_return_greeting(tx, message):
+        result = tx.run("CREATE (a:Greeting) "
+                        "SET a.message = $message "
+                        "RETURN a.message + ', from node ' + id(a)", message=message)
+        return result.single()[0]
+
+    def close(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    greeter = SocialAppDatabase("bolt://127.0.0.1:7687", "neo4j", "password")
+    greeter.print_greeting("hello, world")
+    greeter.close()
