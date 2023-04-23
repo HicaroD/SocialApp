42039b3f1c532053ab7d5bce654958f068100459
HicaroD <hdanrlley1@gmail.com>
2023-04-23 13:08:12 -0300

    refactor(#4): separating register form as a component

--- social_app/lib/app/presenter/pages/register/register_page.dart
+++ social_app/lib/app/presenter/pages/register/register_page.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import '../../widgets/form_field.dart';
+import 'components/register_page_form.dart';
 
 class RegisterPage extends StatelessWidget {
-  RegisterPage({Key? key}) : super(key: key);
-
-  final registerFormKey = GlobalKey<FormState>();
-  final usernameController = TextEditingController();
-  final ageController = TextEditingController();
-  final descriptionController = TextEditingController();
+  const RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,44 +17,7 @@ class RegisterPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Center(
-            child: Form(
-              key: registerFormKey,
-              child: SizedBox(
-                width: screenSize.width * 0.8,
-                child: Column(
-                  children: [
-                    CustomFormField(
-                      labelText: "Username",
-                      controller: usernameController,
-                    ),
-                    CustomFormField(
-                      labelText: "Age",
-                      controller: ageController,
-                      keyboardType: TextInputType.number,
-                    ),
-                    CustomFormField(
-                      labelText: "Description",
-                      controller: descriptionController,
-                    ),
-                    ElevatedButton(
-                      // TODO: interact with API to create a user
-                      onPressed: () {
-                        print(usernameController.text);
-                        print(ageController.text);
-                        print(descriptionController.text);
-                      },
-                      child: const Text("Sign up"),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () => Modular.to.navigate("/"),
-            child: const Text("Already have an account? Sign in"),
-          ),
+          RegisterPageForm(screenSize: screenSize),
         ],
       ),
     );
