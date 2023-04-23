9cb81e3c8a777d0cc9e16a0c0d35de930cafe026
HicaroD <hdanrlley1@gmail.com>
2023-04-23 12:11:15 -0300

    refactor(api): using generator function for getting social app database

--- social_api/main.py
+++ social_api/main.py
@@ -1,16 +1,37 @@
-from fastapi import FastAPI, HTTPException
+import os
+from typing import Generator
+
+from dotenv import dotenv_values
+from fastapi import Depends, FastAPI, HTTPException
 
 from database.connection import SocialAppDatabase
 from models.user import User
 
-app = FastAPI()
-database = SocialAppDatabase("bolt://127.0.0.1:7687", "neo4j", "password")
+CONFIG = dotenv_values(".environment")
 
-# TODO(refactor): the whole API design could be better and simpler.
+app = FastAPI()
+
+
+def get_graph_database() -> Generator[SocialAppDatabase, None, None]:
+    try:
+        database = SocialAppDatabase(
+            CONFIG["NEO4J_URL"],
+            CONFIG["NEO4J_USERNAME"],
+            CONFIG["NEO4J_PASSWORD"],
+        )
+        yield database
+    finally:
+        database.close()
+
+
+# TODO(refactor): the whole API design could be improved
+# Unnecessary code repetition
 
 
 @app.get("/users")
-def get_all_registered_users():
+def get_all_registered_users(
+    database: SocialAppDatabase = Depends(get_graph_database),
+):
     try:
         return database.get_all_users()
     except Exception:
@@ -18,7 +39,10 @@ def get_all_registered_users():
 
 
 @app.post("/users", status_code=201)
-def create_user(user: User):
+def create_user(
+    user: User,
+    database: SocialAppDatabase = Depends(get_graph_database),
+):
     try:
         database.create_user(user)
         return {"detail": "User created successfully"}
@@ -27,7 +51,10 @@ def create_user(user: User):
 
 
 @app.get("/users/{username}/following")
-def get_all_following_users(username: str):
+def get_all_following_users(
+    username: str,
+    database: SocialAppDatabase = Depends(get_graph_database),
+):
     try:
         return database.get_all_following_users(username)
     except Exception:
@@ -35,7 +62,10 @@ def get_all_following_users(username: str):
 
 
 @app.get("/users/{username}/followers")
-def get_all_user_followers(username: str):
+def get_all_user_followers(
+    username: str,
+    database: SocialAppDatabase = Depends(get_graph_database),
+):
     try:
         return database.get_all_users_followers(username)
     except Exception:
@@ -43,7 +73,11 @@ def get_all_user_followers(username: str):
 
 
 @app.post("/users/{first_username}/follow/{second_username}")
-def follow(first_username: str, second_username: str):
+def follow(
+    first_username: str,
+    second_username: str,
+    database: SocialAppDatabase = Depends(get_graph_database),
+):
     try:
         database.follow_user(first_username, second_username)
         return {"detail": f"{first_username} is following {second_username}"}
@@ -52,7 +86,11 @@ def follow(first_username: str, second_username: str):
 
 
 @app.post("/users/{first_username}/unfollow/{second_username}")
-def unfollow(first_username: str, second_username: str):
+def unfollow(
+    first_username: str,
+    second_username: str,
+    database: SocialAppDatabase = Depends(get_graph_database),
+):
     try:
         database.unfollow_user(first_username, second_username)
         return {"detail": f"{first_username} unfollowed {second_username}"}
