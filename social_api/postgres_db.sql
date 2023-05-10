778d2a707c7459faf30b4f2679a739103a251ddf
HicaroD <hdanrlley1@gmail.com>
2023-05-10 19:52:20 -0300

    fix: removing composite primary key

--- social_api/postgres_db.sql
+++ social_api/postgres_db.sql
@@ -39,5 +39,5 @@ create table if not exists socialapp_database.send(
   post_id_fk integer not null references socialapp_database.post(id) on delete cascade,
   comment_id_fk integer not null references socialapp_database.comment(id) on delete cascade,
   user_username_fk varchar(10) not null references socialapp_database.users(username) on delete cascade,
-  primary key (post_id_fk, comment_id_fk)
+  primary key (comment_id_fk)
 );
